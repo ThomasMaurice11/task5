a6f0327bb33c20dc72b67517b95cf4b4a412a072
ThomasMaurice11 <thomasmaurice44@gmail.com>
2023-09-14 09:49:43 +0200

    Initial commit.

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+  img ="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+  title1="smart-watch"
+  price1="20$" ;
+  priceOld1="30$";
+  
+  title2="mobile phone"
+  price2="20$"  ;
+  priceOld2="30$";
+
+  title3="Tv"
+  price3="20$"  ;
+  priceOld3="30$";
+
+  title4="t_shirt"
+  price4="20$"  ;
+  priceOld4="30$";
+
+  title5="shoes"
+  price5="20$"  ;
+  priceOld5="30$";
+
+  title6="airpods"
+  price6="20$"  ;
+  priceOld6="30$";
+
+
+  title7="watch"
+  price7="20$"  ;
+  priceOld7="30$";
+
+  title8="playstation"
+  price8="20$"  ;
+  priceOld8="30$";
+
+  title9="mobile case"
+  price9="20$"  ;
+  priceOld9="30$";
+
+}
